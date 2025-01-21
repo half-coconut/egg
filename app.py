d6952fc77057bb62e2932af1cf36bdf3d06a72df
Half Coconut <584947559@qq.com>
2025-01-21 16:35:54 +0800

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-import os
-app = Flask(__name__)
-app.run(debug=True)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, my first docker images! ' + os.getenv("HOSTNAME") + ''
