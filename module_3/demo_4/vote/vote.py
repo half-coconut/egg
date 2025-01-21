94672b7ce56efb5118776e05f77b1817cc68f698
Half Coconut <584947559@qq.com>
2025-01-21 17:38:27 +0800

    Add files via upload

---
+++ module_3/demo_4/vote/vote.py
@@ -0,0 +1,8 @@
+# from flask import Flask, request
+#
+# app = Flask(__name__)
+#
+# @app.route('/', methods=['GET', 'POST'])
+# def hello():
+#     if request.method == 'POST':
+#         redis = redis.get_redis()
